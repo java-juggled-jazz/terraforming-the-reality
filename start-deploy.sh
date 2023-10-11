af8bf7a68ed376df69692214efd58dcd66581d78
root <kamil.github.conv@gmail.com>
2023-10-11 23:22:32 +0300

    Update

---
+++ start-deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Select any provider to continue / Выберите провайдера для продолжения"
+echo -e "\n""\033[4m""1. Timeweb Cloud""\033[m""\n\n""\033[4m""2. Yandex Cloud""\033[m""\n"
+read provider
+echo $provider
