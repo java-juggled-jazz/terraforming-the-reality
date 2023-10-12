2dd8203207741e7b8994452683568b4f0b539563
root <kamil.github.conv@gmail.com>
2023-10-12 18:29:29 +0300

    Upload

--- start-deploy.sh
+++ start-deploy.sh
@@ -6,6 +6,5 @@ read cpu_cores
 echo "How much RAM is required?"
 read ram
 echo "Select any provider to continue"
-echo -e "\n""\033[4m""1. Timeweb Cloud""\033[m""\n\n""\033[4m""2. Yandex Cloud""\033[m""\n"
+echo -e "\n\033[4m1. Timeweb Cloud\033[m\n\n\033[4m2. Yandex Cloud\033[m\n"
 read provider
-echo $provider
