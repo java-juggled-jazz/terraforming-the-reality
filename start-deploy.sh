69074878ae242ac2c5a0282a2d2247554a7bd083
root <kamil.github.conv@gmail.com>
2023-10-12 13:16:17 +0300

    Update

--- start-deploy.sh
+++ start-deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-echo "Select any provider to continue / Выберите провайдера для продолжения"
+echo "How much Disk space is required?"
+read disk_space
+echo "How many CPU cores is required?"
+read cpu_cores
+echo "How much RAM is required?"
+read ram
+echo "Select any provider to continue"
 echo -e "\n""\033[4m""1. Timeweb Cloud""\033[m""\n\n""\033[4m""2. Yandex Cloud""\033[m""\n"
 read provider
 echo $provider
