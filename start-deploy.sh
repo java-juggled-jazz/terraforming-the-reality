1480f226b89338199df2b10f23665078555c8d0f
root <kamil.github.conv@gmail.com>
2023-10-13 18:40:43 +0300

    Update

--- start-deploy.sh
+++ start-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Let's deploy VM with Kubernetes and Gitlab. Press Ctrl + C to stop."
+echo -e "Let's deploy VM with Kubernetes and Gitlab. \e[4mPress Ctrl + C to stop.\e[0m"
 echo "How much Disk space is required?"
 read disk_space
 echo "How many CPU cores is required?"
@@ -7,5 +7,5 @@ read cpu_cores
 echo "How much RAM is required?"
 read ram
 echo "Select any provider to continue"
-echo -e "\n\033[4m1. Timeweb Cloud\033[m\n\n\033[4m2. Yandex Cloud\033[m\n"
+echo -e "\n\e[4m1. Timeweb Cloud\e[0m\n\n\e[4m2. Yandex Cloud\e[0m\n"
 read provider
